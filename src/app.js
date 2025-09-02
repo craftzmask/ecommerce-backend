c8eb34f5ac71da74efcb8464203c9640a798e410
Khanh Chung <khanh.d.chung@gmail.com>
2025-09-01 18:28:37 -0700

    init project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const s = "Hello World";
+  res.status(200).json({
+    message: "Welcome World",
+    metadata: s.repeat(100000),
+  });
+});
+
+// handling error
+
+module.exports = app;
