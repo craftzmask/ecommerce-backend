f4b3baa4a897b6dfedc83ff02e6fb6590790231b
Khanh Chung <khanh.d.chung@gmail.com>
2025-09-23 16:38:40 -0700

    add discount model

---
+++ src/models/discount.model.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const { Schema, model } = require("mongoose");
+
+const DOCUMENT_NAME = "Discount";
+const COLLECTION_NAME = "Discounts";
+
+const inventorySchema = new Schema(
+  {
+    discount_name: {
+      type: String,
+      required: true,
+    },
+    discount_description: {
+      type: String,
+      required: true,
+    },
+    discount_type: {
+      type: String,
+      required: true,
+      enum: ["fix_amount", "percentage"],
+    },
+    discount_value: {
+      type: Number,
+      required: true,
+    },
+    discount_code: {
+      type: String,
+      required: true,
+    },
+    discount_startDate: {
+      type: Date,
+      required: true,
+    },
+    discount_endDate: {
+      type: Date,
+      required: true,
+    },
+    discount_quantity: {
+      type: Number,
+      rqeuired: true,
+    },
+    discount_usesCount: {
+      type: Number,
+      required: true,
+    },
+    discount_usersUsed: {
+      type: Array,
+      default: [],
+    },
+    discount_maxUsesPerUser: {
+      type: Number,
+      required: true,
+    },
+    discount_minOrderValue: {
+      type: Number,
+      required: true,
+    },
+    discount_shopId: {
+      type: Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    discount_isActive: {
+      type: Boolean,
+      default: true,
+    },
+    discount_appliedTo: {
+      type: String,
+      required: true,
+      enum: ["all", "specific"],
+    },
+    discount_productIds: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
+
+module.exports = model(DOCUMENT_NAME, inventorySchema);
