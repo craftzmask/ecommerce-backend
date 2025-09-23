c50b5544a97f0b0666f4770dbb5b1d2eadd1658f
Khanh Chung <khanh.d.chung@gmail.com>
2025-09-22 22:54:56 -0700

    convert Access Controller from class based to module based

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -3,38 +3,43 @@
 const AccessService = require("../services/access.service");
 const { OK, CREATED } = require("../core/success.response");
 
-class AccessController {
-  static handleRefreshToken = async (req, res) => {
-    new OK({
-      message: "Get tokens successfully",
-      metadata: await AccessService.handleRefreshTokenV2({
-        refreshToken: req.refreshToken,
-        user: req.user,
-        keyStore: req.keyStore,
-      }),
-    }).send(res);
-  };
+const handleRefreshToken = async (req, res) => {
+  new OK({
+    message: "Get tokens successfully",
+    metadata: await AccessService.handleRefreshTokenV2({
+      refreshToken: req.refreshToken,
+      user: req.user,
+      keyStore: req.keyStore,
+    }),
+  }).send(res);
+};
 
-  static logout = async (req, res) => {
-    new OK({
-      message: "Logout successfully!",
-      metadata: await AccessService.logout({ keyStore: req.keyStore }),
-    }).send(res);
-  };
+const logout = async (req, res) => {
+  new OK({
+    message: "Logout successfully!",
+    metadata: await AccessService.logout({ keyStore: req.keyStore }),
+  }).send(res);
+};
 
-  static login = async (req, res) => {
-    new OK({
-      message: "Login successfully!",
-      metadata: await AccessService.login(req.body),
-    }).send(res);
-  };
+const login = async (req, res) => {
+  new OK({
+    message: "Login successfully!",
+    metadata: await AccessService.login(req.body),
+  }).send(res);
+};
 
-  static signUp = async (req, res) => {
-    new CREATED({
-      message: "Registered OK",
-      metadata: await AccessService.signUp(req.body),
-    }).send(res);
-  };
-}
+const signUp = async (req, res) => {
+  new CREATED({
+    message: "Registered OK",
+    metadata: await AccessService.signUp(req.body),
+  }).send(res);
+};
+
+const AccessController = {
+  handleRefreshToken,
+  logout,
+  login,
+  signUp,
+};
 
 module.exports = AccessController;
